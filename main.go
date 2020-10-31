2124cf04984f5287ca5fa9cb6f8b85d36f49b32e
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2020-10-31 00:32:03 +0000

    Add js and html + websocket logic|

--- main.go
+++ main.go
@@ -3,17 +3,21 @@ package main
 import (
 	static "github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/olahol/melody.v1"
 )
 
 func main() {
 	r := gin.Default()
+	m := melody.New()
 
 	r.Use(static.Serve("/", static.LocalFile("./public", true)))
 
 	r.GET("/ws", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
 	})
 
 	r.Run(":5000")
